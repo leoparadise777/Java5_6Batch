1eaa138108f6d1a223bdb8b46e45d5167d49824f
leoparadise777 <leopennylove@gmail.com>
2022-02-23 14:31:33 -0800

    init

--- src/main/java/Day1/Test.java
+++ src/main/java/Day1/Test.java
@@ -1,10 +1,7 @@
+package Day1;
+
 public class Test {
-
     public static void main(String[] args) {
-
         System.out.println("Hi");
-
-
     }
-
 }
---
+++ src/main/java/Day2/Assignment1_13.java
@@ -0,0 +1,33 @@
+package Day2;import java.util.*;
+
+public class Assignment1_13 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            String s = sc.nextLine();
+            if(s.equals("q")){
+                System.out.println("quit");
+                break;
+            }
+            else{
+                int n = Integer.parseInt(s);
+                if(n <= 0){
+                    System.out.println("Error");
+                }
+                else{
+                    System.out.println(doSomthing(n));
+                }
+            }
+        }
+    }
+
+    public static int doSomthing(int n){
+        int res = 0;
+        for(int i = 1; i <= n; i++){
+            res += i;
+        }
+        return res;
+    }
+
+}
